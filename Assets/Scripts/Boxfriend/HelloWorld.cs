90a7f06148fa4f3736a62a198d9b8197eec127b6
Boxfriend <cawills394@gmail.com>
2022-02-20 07:54:30 -0500

    setting up dev environment

---
+++ Assets/Scripts/Boxfriend/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Boxfriend
+{
+    public class HelloWorld
+    {
+        //Just a placeholder while I set up my dev environment
+    }
+}
