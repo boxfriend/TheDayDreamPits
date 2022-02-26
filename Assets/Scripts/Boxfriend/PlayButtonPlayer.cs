1d839e3230c4204c15e8bff37fc007287b5ed1bf
Boxfriend <cawills394@gmail.com>
2022-02-25 20:10:41 -0500

    added volume control and play button fakeouts

---
+++ Assets/Scripts/Boxfriend/PlayButtonPlayer.cs
@@ -0,0 +1,46 @@
+﻿using Random = System.Random;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Boxfriend
+{
+    public class PlayButtonPlayer : MonoBehaviour
+    {
+        [SerializeField] private ChangeScene _sceneChanger;
+        [SerializeField] private RectTransform _rect;
+        [SerializeField] private TMPro.TMP_Text _funnyText;
+
+        [SerializeField] private int _minXPos, _maxXPos;
+        [SerializeField] private int _minYPos, _maxYPos;
+
+        [SerializeField] private List<string> _funnyPhrases;
+
+        private Random _rng;
+
+        private void Awake () => _rng = new Random();
+
+        public void PlayClicked(int sceneNumber)
+        {
+            var rando = _rng.Next(_funnyPhrases.Count + 3);
+
+            if(rando >= _funnyPhrases.Count)
+            {
+                _sceneChanger.MoveToScene(sceneNumber);
+            }
+            else
+            {
+                _rect.anchoredPosition = GetNewPosition();
+                _funnyText.text = _funnyPhrases[rando];
+            }
+        }
+
+        private Vector2 GetNewPosition()
+        {
+            var pos = _rect.anchoredPosition;
+            pos.x = _rng.Next(_minXPos, _maxXPos);
+            pos.y = _rng.Next(_minYPos, _maxYPos);
+
+            return pos;
+        }
+    }
+}
---
+++ Assets/Scripts/Boxfriend/VolumeControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _mixer;
+    [SerializeField] private Slider _slider;
+
+    private void Start ()
+    {
+        var volumeSetting = PlayerPrefs.GetFloat("Volume", 0.75f);
+        _mixer.SetFloat("MasterVolume", ConvertToDecibel(volumeSetting));
+        _slider.value = volumeSetting;
+    }
+
+    public void SetVolume()
+    {
+        var volumeSetting = _slider.value;
+        _mixer.SetFloat("MasterVolume", ConvertToDecibel(volumeSetting));
+        PlayerPrefs.SetFloat("Volume", volumeSetting);
+    }
+
+    private float ConvertToDecibel (float number) => Mathf.Log10(number) * 20;
+}
