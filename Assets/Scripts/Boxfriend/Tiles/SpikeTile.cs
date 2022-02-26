e3c972432fec9c78a953e77b6b2d41b2d61f631f
Boxfriend <cawills394@gmail.com>
2022-02-26 15:11:07 -0500

    added spike functionality and added more rooms

---
+++ Assets/Scripts/Boxfriend/Tiles/SpikeTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Boxfriend.Tiles
+{
+    public class SpikeTile : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        private bool _isActive;
+
+        private void Start () => StartCoroutine(SpikeDelay());
+
+        private IEnumerator SpikeDelay()
+        {
+            var delay = Random.Range(1f, 10f);
+            yield return new WaitForSeconds(delay);
+            ToggleActive();
+        }
+
+        public void ToggleActive()
+        {
+            _isActive = !_isActive;
+            _animator.SetBool("Active", _isActive);
+            StartCoroutine(SpikeDelay());
+        }
+
+        private void OnTriggerEnter2D (Collider2D collision)
+        {
+            if(collision.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(1);
+            }
+        }
+    }
+}
